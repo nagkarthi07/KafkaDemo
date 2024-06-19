28c3fde1de85976b4741e2c99446feea917cb5ab
nagkarthi07 <nagkarthi07>
2024-06-19 13:21:13 -0400

    inital commit

---
+++ src/main/java/Kafka/demo/payLoad/User.java
@@ -0,0 +1,42 @@
+package Kafka.demo.payLoad;
+
+public class User {
+
+    private int id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
